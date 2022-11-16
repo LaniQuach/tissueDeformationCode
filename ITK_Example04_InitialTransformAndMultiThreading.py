1b59af332957577e2337400658a7cd47b285dcf2
lani <laninq04@gmail.com>
2022-11-16 16:43:00 -0500

    Working bSpline parameters for umich logo

--- ITK_Example04_InitialTransformAndMultiThreading.py
+++ ITK_Example04_InitialTransformAndMultiThreading.py
@@ -27,31 +27,45 @@ import numpy as np
 
 
 # Import Images
-fixed_image = itk.imread('data/newTestImage.png', itk.F)
-moving_image = itk.imread('data/newTransformedImage.png', itk.F)
+moving_image = itk.imread('data/newTestImage.png', itk.F)
+fixed_image = itk.imread('data/newTransformedImage.png', itk.F)
 
 # Import Default Parameter Map
 parameter_object = itk.ParameterObject.New()
-resolutions = 3
-parameter_map_rigid = parameter_object.GetDefaultParameterMap('rigid',3)
-parameter_object.AddParameterMap(parameter_map_rigid)
+# resolutions = 3
+# parameter_map_rigid = parameter_object.GetDefaultParameterMap('rigid',3)
+# parameter_object.AddParameterMap(parameter_map_rigid)
 
-# For the bspline default parameter map, an extra argument can be specified that define the final bspline grid spacing in physical space. 
-parameter_map_bspline = parameter_object.GetDefaultParameterMap("bspline", resolutions, 20.0)
-parameter_object.AddParameterMap(parameter_map_bspline)
+# # For the bspline default parameter map, an extra argument can be specified that define the final bspline grid spacing in physical space. 
+# parameter_map_bspline = parameter_object.GetDefaultParameterMap("bspline", resolutions, 20.0)
+# parameter_object.AddParameterMap(parameter_map_bspline)
 
 
 # .. and/or load custom parameter maps from .txt file
 parameter_object.AddParameterFile('data/parameters_BSpline.txt')
 
-# Call registration function with initial transfrom and number of threads
-result_image, result_transform_parameters = itk.elastix_registration_method(
-    fixed_image, moving_image,
-    parameter_object=parameter_object,
-    output_directory = 'output/',
-    log_to_console=False)
+# Load Elastix Image Filter Object
+elastix_object = itk.ElastixRegistrationMethod.New(fixed_image, moving_image)
+elastix_object.SetParameterObject(parameter_object)
 
-moving_image_transformix = itk.imread('data/newTransformedImage.png', itk.F)
+# Set additional options
+elastix_object.SetLogToConsole(True)
+
+# Update filter object (required)
+elastix_object.UpdateLargestPossibleRegion()
+
+# Results of Registration
+result_image = elastix_object.GetOutput()
+result_transform_parameters = elastix_object.GetTransformParameterObject()
+plt.axis('off')
+
+plt.imshow(result_image)
+plt.savefig(r'result11_7.png')
+
+
+#itk.imwrite(result_image, 'output/result_image_11_7.nii')
+
+moving_image_transformix = itk.imread('data/align0.1LAP_05CROPPED_55.png', itk.F)
 
 deformation_field = itk.transformix_deformation_field(moving_image_transformix, result_transform_parameters)
 
@@ -60,36 +74,28 @@ deformation_field = itk.transformix_deformation_field(moving_image_transformix, 
 # elastix_object.UpdateLargestPossibleRegion()
 
 # # Results of Registration
-# result_image = elastix_object.GetOutput()
-# result_transform_parameters = elastix_object.GetTransformParameterObject()
-array = itk.GetArrayFromImage(result_image)
-# plt.axis('off')
-# plt.imshow(result_image)
-# plt.savefig(r'transform11_2.png')
-print(array)
 
-%matplotlib inline
+array = itk.GetArrayFromImage(result_image)
+# print(array)
 
 deformation_field = np.asarray(deformation_field).astype(np.float32)
-
-new = " ".join(str(x) for x in deformation_field)
-print(new)
-
-file = open('output/outputDeformationField.txt')
-file.write(new)
+print(deformation_field)
+file = open('output/outputDeformationField.txt', 'w')
+file.write(" ".join(str(x) for x in deformation_field))
 file.close()
 
-# Plot images
-fig, axs = plt.subplots(2, 2, sharey=True, figsize=[30,30])
+#Plot images
+fig, axs = plt.subplots(1, 2, sharey=True, figsize=[30,30])
 plt.figsize=[100,100]
-axs[0,0].imshow(deformation_field[:,:,1])
-axs[0,0].set_title('Deformation Field X', fontsize=30)
-axs[0,1].imshow(deformation_field[:,:,0])
-axs[0,1].set_title('Deformation Field Y', fontsize=30)
-
+axs[0].imshow(deformation_field[:,:,1])
+axs[0].set_title('Deformation Field X', fontsize=30)
+axs[1].imshow(deformation_field[:,:,0])
+axs[1].set_title('Deformation Field Y', fontsize=30)
+plt.savefig(r'deformation11_7.png')
 
 plt.show()
-plt.savefig(r'deformation11_7.png')
+plt.colorbar()
+
 
 
 
