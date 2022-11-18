4d786a8da231718152b2f22054c3e007b9eb8613
lani <laninq04@gmail.com>
2022-11-18 09:40:16 -0500

    Fix transformation error
    can now fully transform moving image to original image and produce deformation field

--- ITK_Example04_InitialTransformAndMultiThreading.py
+++ ITK_Example04_InitialTransformAndMultiThreading.py
@@ -22,21 +22,13 @@ import numpy as np
 
 
 # Import Images
-moving_image = itk.imread('transformingTestImage/newTestImage.png', itk.F)
-fixed_image = itk.imread('transformingTestImage/newTransformedImage.png', itk.F)
+fixed_image = itk.imread('transformingTestImage/newTestImage.png', itk.F)
+moving_image = itk.imread('transformingTestImage/newTransformedImage.png', itk.F)
 
 # Import Default Parameter Map
 parameter_object = itk.ParameterObject.New()
-# resolutions = 3
-# parameter_map_rigid = parameter_object.GetDefaultParameterMap('rigid',3)
-# parameter_object.AddParameterMap(parameter_map_rigid)
 
-# # For the bspline default parameter map, an extra argument can be specified that define the final bspline grid spacing in physical space. 
-# parameter_map_bspline = parameter_object.GetDefaultParameterMap("bspline", resolutions, 20.0)
-# parameter_object.AddParameterMap(parameter_map_bspline)
-
-
-# .. and/or load custom parameter maps from .txt file
+# Load custom parameter maps from .txt file
 parameter_object.AddParameterFile('data/parameters_BSpline.txt')
 
 # Load Elastix Image Filter Object
@@ -57,25 +49,25 @@ plt.axis('off')
 plt.imshow(result_image)
 #itk.imwrite(result_image, 'output/result_image_11_7.nii')
 
-
 #########Deformation Field#########
-# moving_image_transformix = itk.imread('transformingTestImage/newTransformedImage.png', itk.F)
-# deformation_field = itk.transformix_deformation_field(moving_image_transformix, result_transform_parameters)
+deformation_field = itk.transformix_deformation_field(moving_image, result_transform_parameters)
 
-# array = itk.GetArrayFromImage(result_image)
+array = itk.GetArrayFromImage(result_image)
 
-# deformation_field2 = np.asarray(deformation_field).astype(np.float32)
-# # print(deformation_field)
-# # file = open('output/outputDeformationField.txt', 'w')
-# # file.write(" ".join(str(x) for x in deformation_field))
-# # file.close()
+deformation_field2 = np.asarray(deformation_field).astype(np.float32)
 
-# #Plot images
-# fig, axs = plt.subplots(1, 2, sharey=True, figsize=[30,30])
-# plt.figsize=[100,100]
-# axs[0].imshow(deformation_field[:,:,1])
-# axs[0].set_title('Deformation Field X', fontsize=30)
-# axs[1].imshow(deformation_field[:,:,0])
-# axs[1].set_title('Deformation Field Y', fontsize=30)
+# Write the raw data to a file
+# print(deformation_field)
+# file = open('output/outputDeformationField.txt', 'w')
+# file.write(" ".join(str(x) for x in deformation_field))
+# file.close()
+
+#Plot images
+fig, axs = plt.subplots(1, 2, sharey=True, figsize=[30,30])
+plt.figsize=[100,100]
+axs[0].imshow(deformation_field[:,:,1])
+axs[0].set_title('Deformation Field X', fontsize=30)
+axs[1].imshow(deformation_field[:,:,0])
+axs[1].set_title('Deformation Field Y', fontsize=30)
 
 # plt.colorbar()
